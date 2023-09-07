78ad6065eae23afec9185b78d95d571d8d4310d8
ninjafiveo <mjsekol@live.com>
2023-09-07 09:18:38 -0400

    Changed to a random number.

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ print("Hello World")
 #Add a function that asks the user their name and prints a greeting. 
 
 
-#Add a math function that multiplies two numbers. 
+#Add a function that creates a random number from 1-10 and prints the results to the console. 
